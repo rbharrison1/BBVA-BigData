54b5fddd46f2a7f60de6891b79b1e1cd9b5f01fc
rbharrison1 <rbharrison1@crimson.ua.edu>
2017-02-07 14:34:38 -0600

    Added comments to outline

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,48 @@
+import java.io.Console;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        //*************
+        // READ IN FILE
+        //*************
+
+        //Console c = System.console();
+        //System.out.print("File path: ");
+        //String path = c.readLine();
+
+        //*********************
+        // SPLIT FILE BY RECORD
+        //*********************
+
+
+
+        //***************************
+        // SPLIT EACH RECORD BY FIELD
+        //***************************
+
+
+
+        //************************************************************
+        // ASSIGN EACH FIELD TO CORRESPONDING MEMBER IN QADTAUD OBJECT
+        // ADD OBJECT TO LIST
+        //************************************************************
+
+
+
+        //*********************************
+        // CONVERT FROM JAVA OBJECT TO JSON
+        //*********************************
+
+
+
+        //******************
+        // INSERT TO MONGODB
+        //******************
+
+
+
+
     }
 }
