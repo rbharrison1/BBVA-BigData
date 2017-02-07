7017adb767b5d3734b24b7b8d3a3a46ec03d58aa
rbharrison1 <rbharrison1@crimson.ua.edu>
2017-02-07 15:27:37 -0600

    set up for assigning fields to QADTAUD object

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
-import java.io.Console;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        String line;
+        String[] fields;
+        List<QADTAUD> list = new ArrayList<QADTAUD>();
+        QADTAUD record;
 
         //*************
         // READ IN FILE
@@ -11,25 +18,55 @@ public class Main {
         //Console c = System.console();
         //System.out.print("File path: ");
         //String path = c.readLine();
+        String path = "C:/Users/rharrison15/Desktop/Test Files/shortened.1";
 
-        //*********************
-        // SPLIT FILE BY RECORD
-        //*********************
+        try {
+
+            FileReader fr = new FileReader(path);
+
+            BufferedReader br = new BufferedReader(fr);
+
+            //*********************
+            // SPLIT FILE BY RECORD
+            //*********************
+            while((line = br.readLine()) != null) {
+                //***************************
+                // SPLIT EACH RECORD BY FIELD
+                //***************************
+                fields = line.split("[|]");
+
+                record = null;
+
+                //************************************************************
+                // ASSIGN EACH FIELD TO CORRESPONDING MEMBER IN QADTAUD OBJECT
+                // ADD OBJECT TO LIST
+                //************************************************************
+
+                //1-30 start with TAUD_ENT (Lauryn)
+                record.setTAUD_ENT(fields[0]);
 
 
-
-        //***************************
-        // SPLIT EACH RECORD BY FIELD
-        //***************************
+                //31-60 start with TAUD_COD_TRNS_2 (Danella)
 
 
-
-        //************************************************************
-        // ASSIGN EACH FIELD TO CORRESPONDING MEMBER IN QADTAUD OBJECT
-        // ADD OBJECT TO LIST
-        //************************************************************
+                //61-90 start with TAUD_EVENT9 (Dallas)
 
 
+                //91-120 start with TAUD_ENT_ANALYTIC (Taylor)
+
+
+                //121-142 start with TAUD_AMT_CSHCRE (Ryne)
+
+
+                list.add(record);
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
         //*********************************
         // CONVERT FROM JAVA OBJECT TO JSON
