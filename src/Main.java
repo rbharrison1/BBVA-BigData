668aaf18555118894286a10ba85a933b39312a57
laday <laday@crimson.ua.edu>
2017-02-07 15:40:05 -0600

    Lines 1-30

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,6 +45,36 @@ public class Main {
 
                 //1-30 start with TAUD_ENT (Lauryn)
                 record.setTAUD_ENT(fields[0]);
+                record.setTAUD_URN(fields[1]);
+                record.setTAUD_STP_REG(Date.valueOf(fields[2]));
+                record.setTAUD_BRN_ACCT(fields[3]);
+                record.setTAUD_ACCT_NTNM(fields[4]);
+                record.setTAUD_ACCT_TRM(fields[5]);
+                record.setTAUD_DAY_ACCT(fields[6]);
+                record.setTAUD_DAT_ACCT(fields[7]);
+                record.setTAUD_DAT_OPERATION(fields[8]);
+                record.setTAUD_BRN_LOCATION(fields[9]);
+                record.setTAUD_NETNAME(fields[10]);
+                record.setTAUD_TERMINAL(fields[11]);
+                record.setTAUD_USER_ID(fields[12]);
+                record.setTAUD_SESSION(fields[13]);
+                record.setTAUD_TYP_TERMINAL(fields[14]);
+                record.setTAUD_TERMCODE(fields[15]);
+                record.setTAUD_DAT_TRMS(fields[16]);
+                record.setTAUD_TIM_TRMS(fields[17]);
+                record.setTAUD_CICS(fields[18]);
+                record.setTAUD_NUM_TASK(Double.valueOf(fields[19]));
+                record.setTAUD_COD_START(fields[20]);
+                record.setTAUD_FCC(fields[21]);
+                record.setTAUD_STATUS(fields[22]);
+                record.setTAUD_LNG_TERMINAL(fields[23]);
+                record.setTAUD_COD_APP_1(fields[24]);
+                record.setTAUD_COD_TRNS_2(fields[25]);
+                record.setTAUD_COD_PROGRAM_1(fields[26]);
+                record.setTAUD_PASS_CTRL_2(fields[27]);
+                record.setTAUD_COD_APP_2(fields[28]);
+                record.setTAUD_COD_TRNS_2(fields[29]);
+
 
 
                 //31-60 start with TAUD_COD_TRNS_2 (Danella)
