5e0ed93515e7978b2c850b6c1368e2e97cfe60a0
dlocke15 <dglocke@crimson.ua.edu>
2017-02-20 13:05:01 -0600

    QADTFDC class

---
+++ src/QADTFDC.java
@@ -0,0 +1,105 @@
+import javax.print.DocFlavor;
+
+/**
+ * Created by dlocke15 on 2/15/2017.
+ */
+public class QADTFDC {
+    private String TFDC_DES_COPY;
+    private Double TFDC_NUM_FLD_ID;
+    private Double TFDC_FLD_PST;
+    private String TFDC_FLD_TAG;
+    private String TFDC_DES_FIELD;
+    private String TFDC_TYP_FIELD;
+    private Double TFDC_FLD_LTH;
+    private Double TFDC_DEC_DIG;
+    private String TFDC_DELIMITER;
+    private String TFDC_REQ_FLD;
+    private String TFDC_RUT_VAL;
+    private String TFDC_ARG_SEARCH;
+    private String TFDC_FLD_ENCRYPT;
+    private String TFDC_RESERVED_1;
+
+    public String getTFDC_DES_COPY() {return TFDC_DES_COPY;}
+
+    public void setTFDC_DES_COPY(String TFDC_DES_COPY) {
+        this.TFDC_DES_COPY = TFDC_DES_COPY;
+    }
+
+    public Double getTFDC_NUM_FLD_ID() {return TFDC_NUM_FLD_ID;}
+
+    public void setTFDC_NUM_FLD_ID(Double TFDC_NUM_FLD_ID) {
+        this.TFDC_NUM_FLD_ID = TFDC_NUM_FLD_ID;
+    }
+
+    public Double getTFDC_FLD_PST() {return TFDC_FLD_PST;}
+
+    public void setTFDC_FLD_PST(Double TFDC_FLD_PST) {
+        this.TFDC_FLD_PST = TFDC_FLD_PST;
+    }
+
+    public String getTFDC_FLD_TAG() {return TFDC_FLD_TAG;}
+
+    public void setTFDC_FLD_TAG(String TFDC_FLD_TAG) {
+        this.TFDC_FLD_TAG = TFDC_FLD_TAG;
+    }
+
+    public String getTFDC_DES_FIELD() {return TFDC_DES_FIELD;}
+
+    public void setTFDC_DES_FIELD(String TFDC_DES_FIELD) {
+        this.TFDC_DES_FIELD = TFDC_DES_FIELD;
+    }
+
+    public String getTFDC_TYP_FIELD() {return TFDC_TYP_FIELD;}
+
+    public void setTFDC_TYP_FIELD(String TFDC_TYP_FIELD) {
+        this.TFDC_TYP_FIELD = TFDC_TYP_FIELD;
+    }
+
+    public Double getTFDC_FLD_LTH() {return TFDC_FLD_LTH;}
+
+    public void setTFDC_FLD_LTH(Double TFDC_FLD_LTH) {
+        this.TFDC_FLD_LTH = TFDC_FLD_LTH;
+    }
+
+    public Double getTFDC_DEC_DIG() {return TFDC_DEC_DIG;}
+
+    public void setTFDC_DEC_DIG(Double TFDC_DEC_DIG) {
+        this.TFDC_DEC_DIG = TFDC_DEC_DIG;
+    }
+
+    public String getTFDC_DELIMITER() {return TFDC_DELIMITER;}
+
+    public void setTFDC_DELIMITER(String TFDC_DELIMITER) {
+        this.TFDC_DELIMITER = TFDC_DELIMITER;
+    }
+
+    public String getTFDC_REQ_FLD() {return TFDC_REQ_FLD;}
+
+    public void setTFDC_REQ_FLD(String TFDC_REQ_FLD) {
+        this.TFDC_REQ_FLD = TFDC_REQ_FLD;
+    }
+
+    public String getTFDC_RUT_VAL() {return TFDC_RUT_VAL;}
+
+    public void setTFDC_RUT_VAL(String TFDC_RUT_VAL) {
+        this.TFDC_RUT_VAL = TFDC_RUT_VAL;
+    }
+
+    public String getTFDC_ARG_SEARCH() {return TFDC_ARG_SEARCH;}
+
+    public void setTFDC_ARG_SEARCH(String TFDC_ARG_SEARCH) {
+        this.TFDC_ARG_SEARCH = TFDC_ARG_SEARCH;
+    }
+
+    public String getTFDC_FLD_ENCRYPT() {return TFDC_FLD_ENCRYPT;}
+
+    public void setTFDC_FLD_ENCRYPT(String TFDC_FLD_ENCRYPT) {
+        this.TFDC_FLD_ENCRYPT = TFDC_FLD_ENCRYPT;
+    }
+
+    public String getTFDC_RESERVED_1() {return TFDC_RESERVED_1;}
+
+    public void setTFDC_RESERVED_1(String TFDC_RESERVED_1) {
+        this.TFDC_RESERVED_1 = TFDC_RESERVED_1;
+    }
+}
